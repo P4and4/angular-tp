7acea1aa1cb59a39656d8a5cd6b46435c88b3a5f
NarjesTaghlet <narjes.taghlet@insat.ucar.tn>
2023-12-07 10:36:16 +0100

    delete

--- src/app/cvTech/detail/detail.component.ts
+++ src/app/cvTech/detail/detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CvService } from '../cv.service';
 import {Personne} from "../../Model/Personne";
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
@@ -10,9 +10,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DetailComponent implements OnInit {
   personne! : Personne ;
-  constructor(  private cvService:
-                  CvService,
-                private activatedRoute: ActivatedRoute
+  constructor(  private cvService:CvService,
+                private activatedRoute: ActivatedRoute,
+                private router: Router,
+                private toastr: ToastrService
 
     ) {
   }
@@ -23,6 +24,20 @@ export class DetailComponent implements OnInit {
         this.personne = this.cvService.getPersonneById(params['id']);
       }
     )
+  }
+
+  delete() {
+    try {
+      // Call the service to delete the person locally
+      this.cvService.deletePersonneById(this.personne.id);
+
+      // Redirect to the CVComponent
+      this.router.navigate(['cv']);
+    } catch (error) {
+      this.toastr.error("Problème de suppression, veuillez contacter l'admin");
+      console.log(error)
+
+    }
   }
 }
 
